29f788414ee1ed02e87bab3d5879c4a278bc2941
xingstar <2115438238@qq.com>
2024-08-23 13:35:04 +0800

    Site updated: 2024-08-23 13:35:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/19/hello-world/","2024/08/21/Git学习笔记-一/","2024/08/23/Git学习笔记-二/","2024/08/22/python基础学习总结(七)/"];function toRandomPost(){
+var posts=["2024/08/23/Git学习笔记-二/","2024/08/19/hello-world/","2024/08/21/Git学习笔记-一/","2024/08/22/python基础学习总结-七/","2024/08/23/python基础学习总结-八/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

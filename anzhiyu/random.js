dfa3d770c030c4cb430de511cdd2d25ccc4c0c8d
xingstar <2115438238@qq.com>
2024-08-27 15:05:28 +0800

    Site updated: 2024-08-27 15:05:27

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/21/Git学习笔记-一/","2024/08/26/Git学习笔记-四/","2024/08/25/Git学习笔记-三/","2024/08/23/Git学习笔记-二/","2024/08/19/hello-world/","2024/08/25/python基础学习总结-九/","2024/08/26/python基础学习总结-十/","2024/08/22/python基础学习总结-七/","2024/08/23/python基础学习总结-八/"];function toRandomPost(){
+var posts=["2024/08/21/Git学习笔记-一/","2024/08/25/Git学习笔记-三/","2024/08/23/Git学习笔记-二/","2024/08/27/css/","2024/08/23/python基础学习总结-八/","2024/08/26/Git学习笔记-四/","2024/08/22/python基础学习总结-七/","2024/08/19/hello-world/","2024/08/25/python基础学习总结-九/","2024/08/26/python基础学习总结-十/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
